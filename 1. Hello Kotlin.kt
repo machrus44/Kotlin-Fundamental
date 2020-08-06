33ddc322fe7a39cdfb48ce8e60486e2ff23c5c6b
machrus44 <68941349+machrus44@users.noreply.github.com>
2020-08-06 20:00:20 +0700

    Add files via upload

---
+++ 1. Hello Kotlin.kt
@@ -0,0 +1,11 @@
+fun main () {
+    val name = "haykal"
+    val hobi = "Main game dan bersepeda"
+
+    print("Halo nama saya   :")
+    println(name)
+    print("Hobi saya adalah :")
+    println(hobi)
+    print(if (true)"always true" else "always false")
+
+}
---
+++ 3. Characters.kt
@@ -0,0 +1,15 @@
+fun main(){
+    var huruf = 'z'
+
+    println("Huruf " + huruf--)
+    println("Huruf " + huruf--)
+    println("Huruf " + huruf++)
+    huruf -= 6;
+    println("Huruf " + huruf--)
+    println("Huruf " + huruf++)
+    println("Huruf " + huruf--)
+    huruf += 6
+    println("Huruf " + huruf++)
+    println("Huruf " + huruf++)
+    println("Huruf " + huruf++)
+}
